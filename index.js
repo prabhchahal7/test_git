97ed06ed0d62c1ddea7ffe894376e11e53673ca2
Prabh Chahal <prabhchahal@Prabhs-Air>
2023-09-11 23:09:50 +0530

    ok

--- index.js
+++ index.js
@@ -2,7 +2,3 @@ console.log("hello");
 
 console.log("this is in first_brr");
 console.log("moving to master branch");
-
-console.log("this is in second_brr");
-
-console.log("this is in third_brr");
